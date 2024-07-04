0dd600e5179fa9d47a836c1be3f8257822caf323
sss <>
2024-07-04 15:29:28 +0900

    Added JavaScript text

---
+++ script.js
@@ -0,0 +1,25 @@
+// 変数
+let unko = "hello world";
+
+unko = "hello world2"
+
+console.log(unko);
+
+
+// 定数
+const bigUnko = "Hello world";
+
+console.log(bigUnko);
+
+// 配列
+let inoki =["いーち","にーい","さーん"];
+
+console.log(inoki[1]);
+
+// ループ文
+let index =0;
+while(index < 5){
+    // ここに繰り返したい命令分
+    console.log(index)
+    index++;   
+}
